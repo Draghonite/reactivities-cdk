2a8e0c427b49ed39b13bd8bbb85b8562afafc539
Ivan Mercedes <ivan.mercedes@hybriddatasecurity.com>
2022-02-04 00:07:48 -0500

    added build stage w/ placeholder commands

--- bin/reactivities-cdk.ts
+++ bin/reactivities-cdk.ts
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { ReactivitiesCICDPipelineStack } from '../lib/reactivities-cicd-pipeline-stack';
+import { CICDPipelineStack } from '../lib/reactivities-cicd-pipeline-stack';
 
 const app = new cdk.App();
-console.log(process.env.CDK_DEFAULT_ACCOUNT, process.env.CDK_DEFAULT_REGION);
-new ReactivitiesCICDPipelineStack(app, 'reactivities-cicd-pipeline-stack', {});
+new CICDPipelineStack(app, 'ReactivitiesCICDPipelineStack');
