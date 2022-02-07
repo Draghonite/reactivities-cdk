9fc62e4866ee1c6a9523006d07f9f50838fca28d
Ivan Mercedes <ivan.mercedes@sidemotion.com>
2022-02-07 15:19:08 +0000

    using default subnet config w/ custom vpc ip range; added service and alb config in ecs

--- lib/reactivities-cicd-pipeline-stack.ts
+++ lib/reactivities-cicd-pipeline-stack.ts
@@ -8,67 +8,75 @@ import { Construct } from 'constructs';
 import { LinuxBuildImage } from 'aws-cdk-lib/aws-codebuild';
 import { Repository } from 'aws-cdk-lib/aws-ecr';
 import { SubnetType, Vpc } from 'aws-cdk-lib/aws-ec2';
-import { Cluster, ContainerImage, FargateTaskDefinition, Secret } from 'aws-cdk-lib/aws-ecs';
+import { Cluster, ContainerImage, FargateService, FargateTaskDefinition, ListenerConfig, Secret } from 'aws-cdk-lib/aws-ecs';
 import { RuntimeFamily } from 'aws-cdk-lib/aws-lambda';
+import { ApplicationLoadBalancer, ApplicationProtocol } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 
 export class ReactivitiesCICDPipelineStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
         // provision ECR
-        const repository = new Repository(this, "ReactivitiesRepository", {
-            repositoryName: "reactivities-repository"
+        const repository = new Repository(this, 'ReactivitiesRepository', {
+            repositoryName: 'reactivities-repository'
         });
 
         // provision ECS
-        const vpc = new Vpc(this, "ReactivitiesVPC", {
-            vpcName: "ReactivitiesVPC",
-            cidr: "10.0.0.0/24",
-            subnetConfiguration: [
-                {
-                    cidrMask: 24,
-                    name: 'ingress',
-                    subnetType: SubnetType.PUBLIC
-                },
-                {
-                    cidrMask: 24,
-                    name: 'application',
-                    subnetType: SubnetType.PRIVATE_ISOLATED,
-                }
-            ]
+        const vpc = new Vpc(this, 'ReactivitiesVPC', {
+            vpcName: 'ReactivitiesVPC',
+            cidr: '10.0.0.0/24'
         });
-        const containerService = new Cluster(this, "ReactivitiesECSCluster", {
-            clusterName: "ReactivitiesECSCluster",
+        const cluster = new Cluster(this, 'ReactivitiesECSCluster', {
+            clusterName: 'ReactivitiesECSCluster',
             vpc: vpc
         });
-        const fargateTaskDefinition = new FargateTaskDefinition(this, "ReactivitiesFargateDefinition", {
+        const fargateTaskDefinition = new FargateTaskDefinition(this, 'ReactivitiesFargateDefinition', {
             cpu: 256,
             memoryLimitMiB: 512,
-            family: "ReactivitiesFargateDefinition"
+            family: 'ReactivitiesFargateDefinition'
         });
         // let cloudinaryAPISecret: cdk.aws_secretsmanager.Secret;
         // let cloudinaryCloudName: cdk.aws_secretsmanager.Secret;
         // let cloudinaryAPIKey: cdk.aws_secretsmanager.Secret;
         // let tokenKey: cdk.aws_secretsmanager.Secret;
         // let databaseURL: cdk.aws_secretsmanager.Secret;
-        const container = fargateTaskDefinition.addContainer("ReactivitiesContainer", {
-            containerName: "ReactivitiesContainer",
-            image: ContainerImage.fromEcrRepository(repository, "latest"),
+        const container = fargateTaskDefinition.addContainer('ReactivitiesContainer', {
+            containerName: 'ReactivitiesContainer',
+            image: ContainerImage.fromEcrRepository(repository, 'latest'),
             cpu: 256,
             memoryLimitMiB: 512,
             environment: {
-                "ASPNETCORE_ENVIRONMENT": "Production"
+                'ASPNETCORE_ENVIRONMENT': 'Production'
             },
             // secrets: {
-            //     Cloudinary__ApiSecret: Secret.fromSecretsManager(cloudinaryAPISecret, "Cloudinary__ApiSecret"),
-            //     Cloudinary__CloudName: Secret.fromSecretsManager(cloudinaryCloudName, "Cloudinary__CloudName"),
-            //     Cloudinary__ApiKey: Secret.fromSecretsManager(cloudinaryAPIKey, "Cloudinary__ApiKey"),
-            //     TokenKey: Secret.fromSecretsManager(tokenKey, "TokenKey"), // TODO: create specifically for the application ("ReactivitiesTokenKey")
-            //     DATABASE_URL: Secret.fromSecretsManager(databaseURL, "DATABASE_URL") // TODO: create specifically for the application ("REACTIVITIES_DATABASE_URL")
+            //     Cloudinary__ApiSecret: Secret.fromSecretsManager(cloudinaryAPISecret, 'Cloudinary__ApiSecret'),
+            //     Cloudinary__CloudName: Secret.fromSecretsManager(cloudinaryCloudName, 'Cloudinary__CloudName'),
+            //     Cloudinary__ApiKey: Secret.fromSecretsManager(cloudinaryAPIKey, 'Cloudinary__ApiKey'),
+            //     TokenKey: Secret.fromSecretsManager(tokenKey, 'TokenKey'), // TODO: create specifically for the application ('ReactivitiesTokenKey')
+            //     DATABASE_URL: Secret.fromSecretsManager(databaseURL, 'DATABASE_URL') // TODO: create specifically for the application ('REACTIVITIES_DATABASE_URL')
             // },
             portMappings: [{ containerPort: 80 }]
         });
-        // TODO: needs at least a service and an ALB
+        const service = new FargateService(this, 'ReactivitiesService', {
+            cluster: cluster,
+            taskDefinition: fargateTaskDefinition,
+            desiredCount: 1,
+            minHealthyPercent: 0,
+            maxHealthyPercent: 200
+        });
+        const loadBalander = new ApplicationLoadBalancer(this, 'ReactivitesALB', {
+            vpc: vpc,
+            internetFacing: true
+        });
+        const listener = loadBalander.addListener('ReactivitiesALBListener', { port: 80 });
+        service.registerLoadBalancerTargets({
+            containerName: 'ReactivitiesContainer',
+            containerPort: 80,
+            newTargetGroupId: 'ReactivitiesTG',
+            listener: ListenerConfig.applicationListener(listener, {
+                protocol: ApplicationProtocol.HTTPS
+            })
+        });
 
         // create the pipeline for CI/CD and its stages
         /*
@@ -102,23 +110,23 @@ export class ReactivitiesCICDPipelineStack extends cdk.Stack {
         // #region Build
         const buildOutput = new codepipeline.Artifact('BuildArtifact');
         const codeBuildProject = new codebuild.Project(this, 'ReactivitiesBuildProject', {
-            projectName: "ReactivitiesBuildProject",
+            projectName: 'ReactivitiesBuildProject',
             environment: {
                 buildImage: LinuxBuildImage.AMAZON_LINUX_2_3,
                 privileged: true
             },
             logging: {
                 cloudWatch: {
-                    logGroup: new cdk.aws_logs.LogGroup(this, "ReactivitiesBuildProjectLogGroup", {
-                        logGroupName: "/aws/codebuild/ReactivitiesBuildProject"
+                    logGroup: new cdk.aws_logs.LogGroup(this, 'ReactivitiesBuildProjectLogGroup', {
+                        logGroupName: '/aws/codebuild/ReactivitiesBuildProject'
                     })
                 }
             },
             artifacts: codebuild.Artifacts.s3({
-                bucket: new s3.Bucket(this, "ReactivitiesBucket", {
-                    bucketName: "reactivities-bucket"
+                bucket: new s3.Bucket(this, 'ReactivitiesBucket', {
+                    bucketName: 'reactivities-bucket'
                 }),
-                identifier: "ReactivitiesBuildProject"
+                identifier: 'ReactivitiesBuildProject'
             }),
             buildSpec: codebuild.BuildSpec.fromObject({
                 version: '0.2',
@@ -152,7 +160,7 @@ export class ReactivitiesCICDPipelineStack extends cdk.Stack {
         const buildAction = new codepipeline_actions.CodeBuildAction({
             actionName: 'Build',
             project: codeBuildProject,
-            variablesNamespace: "BuildVariables",
+            variablesNamespace: 'BuildVariables',
             input: sourceOutput,
             outputs: [buildOutput]
         });
